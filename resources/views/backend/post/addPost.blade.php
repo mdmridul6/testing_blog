3dc9fd79befbcc7c806eb7922fba1b02f01b8eda
Md Mridul <70252226+mdmridul6@users.noreply.github.com>
2021-03-31 21:29:10 +0600

    Update addPost.blade.php

--- resources/views/backend/post/addPost.blade.php
+++ resources/views/backend/post/addPost.blade.php
@@ -12,18 +12,18 @@
             @csrf
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" name="title" class="form-control">
+                <input type="text" name="title" class="form-control" value="{{old('title')}}">
             </div>
             <div class="form-group">
-                <label for="title">Desctription</label>
-                <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
+                <label for="description">Desctription</label>
+                <textarea name="description" id="" cols="30" rows="10" class="form-control" value="">{{old('description')}}</textarea>
             </div>
             <div class="form-group">
                 <label for="category">Category</label>
                 <select name="category" id="" class="custom-select">
                     <option value="null" selected disabled>Select Category</option>
                     @foreach ($categories as $category)
-                    <option value="{{$category->id}}">{{$category->name}}</option>
+                    <option   @if ($category->id == old('category')) selected @endif value="{{$category->id}}">{{$category->name}}</option>
 
                     @endforeach
                 </select>
@@ -32,8 +32,8 @@
                 <label for="ststus">Status</label>
                 <select name="ststus" id="" class="custom-select">
                     <option value="null" selected disabled>Select Status</option>
-                    <option value="0">Active</option>
-                    <option value="1">Deactive</option>
+                    <option @if (old('status')  == "0") selected @endif value="0">Active</option>
+                    <option @if (old('status')  == "1") selected @endif value="1">Deactive</option>
                 </select>
             </div>
 
